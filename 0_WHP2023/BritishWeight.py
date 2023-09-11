8a3d06fe59a844fb10958674dcdbdcbd1f2d06c3
Tobias Hernandez Perez <tobi@Tobiass-MacBook-Pro.local>
2023-09-11 15:46:19 +0200

    docs

--- 0_WHP2023/BritishWeight.py
+++ 0_WHP2023/BritishWeight.py
@@ -2,9 +2,21 @@ class BritishWeight:
     _pounds: int = 0
 
     def __init__(self, pounds=0, stones=0):
+        """
+        constructor
+        automatically calculates stones from pounds
+        :param pounds: pounds
+        :param stones: 14 pounds
+        """
         self._pounds = int(stones) * 14 + int(pounds)
 
     def __add__(self, other):
+        """
+        adds either two BritishWeights
+        or a BritishWeight with an int
+        :param other:
+        :return:
+        """
         if isinstance(other, int):
             return BritishWeight(self._pounds + other)
 
@@ -12,9 +24,18 @@ class BritishWeight:
             return BritishWeight(self._pounds + other._pounds)
 
     def __radd__(self, other):
+        """
+        implements right add
+        :param other:
+        :return:
+        """
         return self + other
 
     def __str__(self) -> str:
+        """
+        toString method
+        :return:
+        """
         return str(self._pounds) + "lb = " + str(self._pounds//14) + "st " + str(self._pounds%14) + "lb"
 
 
