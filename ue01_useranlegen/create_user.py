da53ee222bb3f223278ba60174890223b7f7a14d
Tobias Hernandez Perez <9182@htl.rennweg.at>
2023-10-17 14:12:12 +0200

    split method create_user into multiple methods, added logger

--- ue01_useranlegen/create_user.py
+++ ue01_useranlegen/create_user.py
@@ -11,8 +11,10 @@ Als Login-Name wird der Familienname verwendet:
 """
 import logging
 import random
+import sys
 import unicodedata
 import re
+from logging.handlers import RotatingFileHandler
 
 from openpyxl.reader.excel import load_workbook
 from create_class import generate_password
@@ -51,12 +53,17 @@ def read_excel(file_path: str):
 
 
 def create_users(filename: str):
+    """
+    reads user data from excel file
+    yields user data and password
+    :param filename: excel file
+    :return:
+    """
     logger.debug(f"generating users from: {filename}")
 
     last_names = []
     special_char_map = {ord('ä'): 'ae', ord('ü'): 'ue', ord('ö'): 'oe', ord('ß'): 'ss',
                         ord('Ä'): 'Ae', ord('Ü'): 'Ue', ord('Ö'): 'Oe'}
-    # print(special_char_map)
 
     excel = read_excel(filename)
     excel.__next__()
@@ -69,30 +76,51 @@ def create_users(filename: str):
 
         name_pattern = "^[A-Za-z][A-Za-z0-9._-]+$"
         if not re.search(name_pattern, first_name) or not re.search(name_pattern, last_name):
-            # print(first_name, last_name)
             continue
 
-        print(user_name)
-
-        if last_name in last_names:
-            logger.debug("lastname '" + last_name + "' already exists -> appending number")
-            last_names.append(last_name)
-            last_name = last_name + str(last_names.count(last_name) - 1)
-        else:
-            last_names.append(last_name)
+        last_name = convert_multiple_last_names(last_name, last_names)
 
         logger.debug(f"yielding class user: {first_name}")
+        yield from create_user(class_name, first_name, group_name, last_name, user_name)
 
-        if class_name:
-            home_directory = f"/home/klassen/{class_name}/{last_name}/"
-        else:
-            home_directory = f"/home/lehrer/{last_name}/"
-        yield (home_directory,
-               first_name,
-               group_name,
-               class_name,
-               user_name,
-               generate_password(first_name, last_name, str(class_name)))
+
+def create_user(class_name, first_name, group_name, last_name, user_name):
+    """
+    yields given user data and defines home directory
+    :param class_name:
+    :param first_name:
+    :param group_name:
+    :param last_name:
+    :param user_name:
+    :return:
+    """
+    if class_name:
+        home_directory = f"/home/klassen/{class_name}/{last_name}/"
+    else:
+        home_directory = f"/home/lehrer/{last_name}/"
+    yield (home_directory,
+           first_name,
+           group_name,
+           class_name,
+           user_name,
+           generate_password(first_name, last_name, str(class_name)))
+
+
+def convert_multiple_last_names(last_name, last_names):
+    """
+    in case of a reoccurring last name
+    appends iterating number
+    :param last_name:
+    :param last_names:
+    :return:
+    """
+    if last_name in last_names:
+        logger.debug("lastname '" + last_name + "' already exists -> appending number")
+        last_names.append(last_name)
+        last_name = last_name + str(last_names.count(last_name) - 1)
+    else:
+        last_names.append(last_name)
+    return last_name
 
 
 def shave_marks(txt):
@@ -106,6 +134,13 @@ def shave_marks(txt):
 
 
 if __name__ == '__main__':
+    logger.setLevel(logging.DEBUG)
+    rot_file_handler = RotatingFileHandler('create_user.log', maxBytes=10_000, backupCount=5)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    logger.addHandler(rot_file_handler)
+    logger.addHandler(stream_handler)
+
     for user in create_users("Namen.xlsx"):
+        print(user)
         pass
         # print(user)
