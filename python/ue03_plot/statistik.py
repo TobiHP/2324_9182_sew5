2651c3dc3fd7230e7ad50903cf12cd9bc7e5cb55
Tobias Hernandez Perez <9182@htl.rennweg.at>
2024-01-20 20:11:25 +0100

    ue03 plot, create scatter

--- python/ue03_plot/statistik.py
+++ python/ue03_plot/statistik.py
@@ -1,10 +1,41 @@
 import argparse
 import logging
 import sys
+from datetime import datetime
 from logging.handlers import RotatingFileHandler
 from subprocess import Popen, PIPE
 
+import matplotlib.pyplot as plt
+
 logger = logging.getLogger("statistik_logger")
+
+
+def create_git_scatter():
+    git_log = ["git", "log", "--pretty=format:%h|%an|%ad|%s", "--date=format-local:%Y-%m-%d %H:%M:%S"]
+    process = Popen(git_log, stdout=PIPE, stderr=PIPE, text=True)
+    out, err = process.communicate()
+
+    log_entries = out.splitlines()
+
+    commits_by_weekday_hour = {}
+
+    for entry in log_entries:
+        entry_components = entry.split('|')
+
+        commit_date_time = entry_components[2]
+        commit_datetime = datetime.strptime(commit_date_time, "%Y-%m-%d %H:%M:%S")
+
+        weekday = commit_datetime.weekday()
+        hour = commit_datetime.hour
+
+        key = (weekday, hour)
+        commits_by_weekday_hour[key] = commits_by_weekday_hour.get(key, 0) + 10
+
+    weekdays, hours = zip(*commits_by_weekday_hour.keys())
+    counts = list(commits_by_weekday_hour.values())
+
+    plt.scatter(hours, weekdays, s=counts, c='blue', alpha=0.5)
+    plt.show()
 
 
 def create_logger():
@@ -43,9 +74,6 @@ def parse_args():
 
 
 if __name__ == '__main__':
-    git_log = ["git", "log"]
-    process = Popen(git_log, stdout=PIPE, stderr=PIPE, text=True)
-    out, err = process.communicate()
-
-    print(out)
+    create_git_scatter()
+    # print(out)
     # parse_args()
